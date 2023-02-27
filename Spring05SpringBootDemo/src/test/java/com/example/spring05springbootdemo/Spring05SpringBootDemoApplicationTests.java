f7635e988df118990e0378b2cb8b1dcc894f6bc4
orhunakkan <orhunakkan@outlook.com>
2023-02-27 13:39:57 -0600

    Springboot project creation and concepts

---
+++ Spring05SpringBootDemo/src/test/java/com/example/spring05springbootdemo/Spring05SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring05springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring05SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
