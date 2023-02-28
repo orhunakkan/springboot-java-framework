dd5f62ccd15232cf45d2ea3eb619785f20882dfd
orhunakkan <orhunakkan@outlook.com>
2023-02-28 01:17:11 -0600

    MVC Controller started

---
+++ Spring06MVCController/src/test/java/com/example/spring06mvcontroller/Spring06mvcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring06mvcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring06mvcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
